db4938c4cabaeacd40e50c6f68dfb43372617e59
emb <emb@catfact.net>
2022-03-16 17:20:20 -0700

    run, plots

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from plot_baseline import *
 
-plot_data("captures/data_softcut_20220315_235751.csv",
-          "plots/data_softcut_20220315_235751.svg")
+# plot_data("captures/data_softcut_20220315_235751.csv",
+#           "plots/data_softcut_20220315_235751.svg")
+
+
+plot_data("captures/data_softcut_20220316_231750.csv",
+          "plots/data_softcut_20220316_231750.svg")
+
